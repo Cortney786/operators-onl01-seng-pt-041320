2b5f0b6f690485b2ffa7002eb82f6a0aa4d88af6
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-15 17:29:17 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-if speed < 40 || speed > 60
+if speed >60 || speed < 40
  return true
  else
  return false
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-if speed < 40 || speed > 60 ? true : false
+speed < 40 || speed > 60 ? true : false
 end
 	
 
